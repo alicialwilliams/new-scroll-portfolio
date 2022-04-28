2445d51d44dcc4e8d4721469083e4edf9cd8acd0
alicialwilliams <63070379+alicialwilliams@users.noreply.github.com>
2022-04-28 14:01:39 -0400

    added env and gitignore

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require("express")
 const cors = require("cors")
 const path = require("path")
 
-const contactRoute = require('./routes/contactRoute')
+const contactRoute = require('./route/contactRoute')
 
 const app = express();
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors("/", contactRoute));
 
-const port = process.env.PORT || 5000;
-app.listen(port, console.log(`server listening to port 5000`))
+const port = process.env.PORT || 6000;
+app.listen(port, console.log(`Server listening to PORT ${port}.`));
