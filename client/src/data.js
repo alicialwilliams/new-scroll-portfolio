a087f1d6782bf470b8fbba79ad2508dd2476c793
alicialwilliams <63070379+alicialwilliams@users.noreply.github.com>
2022-05-20 14:26:05 -0400

    setup data for projeccts and images for contact styling.

---
+++ client/src/data.js
@@ -0,0 +1,41 @@
+export const projects = [
+    {
+        id: 1,
+        img: 'images/capstoneLandingMap.png',
+        link: 'https://kibblesandritz.herokuapp.com/'
+    },
+    {
+        id: 2,
+        img: 'images/pigLatinFullPage.png',
+        link: 'https://kibblesandritz.herokuapp.com/'
+    },
+    // {
+    //     id: 3,
+    //     img: 'images/capstoneProj.jpg',
+    //     link: 'https://kibblesandritz.herokuapp.com/'
+    // },
+    // {
+    //     id: 4,
+    //     img: 'images/capstoneProj.jpg',
+    //     link: 'https://kibblesandritz.herokuapp.com/'
+    // },
+    // {
+    //     id: 5,
+    //     img: 'images/capstoneProj.jpg',
+    //     link: 'https://kibblesandritz.herokuapp.com/'
+    // },
+    // {
+    //     id: 6,
+    //     img: 'images/capstoneProj.jpg',
+    //     link: 'https://kibblesandritz.herokuapp.com/'
+    // }
+]
+
+
+/* STATIC RESUME DATA FOR THE LABELS*/
+export const resumeBullets = [
+    { label: "Education", logoSrc: "images/resume/education.png" },
+    { label: "Work History", logoSrc: "images/resume/work.png" },
+    { label: "Programming Skills", logoSrc: "images/resume/programming.png" },
+    { label: "Projects", logoSrc: "images/resume/projects.png" },
+];
