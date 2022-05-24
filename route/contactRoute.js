71bd66be1fa582d860188d5ba40b913c15a9a4a3
alicialwilliams <63070379+alicialwilliams@users.noreply.github.com>
2022-05-24 00:09:50 -0400

    restored manual proxy.

--- route/contactRoute.js
+++ route/contactRoute.js
@@ -11,11 +11,9 @@ router.post('/contact', (req, res) => {
     let smtpTransporter = nodemailer.createTransport({
         service: 'Gmail',
         port: 465,
-        secure: true,
         auth: {
             user: process.env.EMAIL,
             pass: process.env.WORD,
-            // pass: process.env.WORD2
         }
     })
     let mailOptions = {
