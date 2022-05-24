d84f1fff8be34ded1058bbb868e7cb82085fbef4
alicialwilliams <63070379+alicialwilliams@users.noreply.github.com>
2022-05-23 23:54:01 -0400

    changed favicon and title

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Helmet } from "react-helmet";
 import PortfolioContainer from "./PortfolioContainer/PortfolioContainer";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -6,6 +7,12 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Alicia Can Create</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Developer Porfolio" />
+      </Helmet>
       <ToastContainer />
       <PortfolioContainer />
     </div>
